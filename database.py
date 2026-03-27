11080b2fd79689ee2b4efa41fa32830435da751f
Pessi Vehmas <pessi.vehmas@gmail.com>
2026-03-27 19:15:40 +0200

    created database connection using g functionalities to bridge between app and schema

--- database.py
+++ database.py
@@ -1,2 +1,21 @@
 import sqlite3
 from flask import g
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("database.db")
+        g.db.row_factory = sqlite3.Row
+        g.db.execute("PRAGMA foreign_keys = ON")
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+def init_db(app):
+    with app.app_contect():
+        db = get_db()
+        with app.open_resource("schema.sql") as f:
+            db.executescript(f.read().decode("utf8"))
+        db.commit()
